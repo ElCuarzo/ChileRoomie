7be16e5e411f2a4f745e4b096f79f5c1e70db975
Sherlock898 <aguayo.sebastian.r@gmail.com>
2024-05-01 00:03:07 -0400

    imeplementado modelo basico de direccion y arriendo

---
+++ src/main/java/com/chileroomie/chileroomieapp/modelos/Arriendo.java
@@ -0,0 +1,71 @@
+package com.chileroomie.chileroomieapp.modelos;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "arriendos")
+public class Arriendo {
+    // TODO: quizás hay que cambiar esto, 
+    enum Estado {DISPONIBLE, ARRENDADO, MANTENCION}
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private Estado tipo_de_arriendo;
+    private boolean amoblado;
+    private int precio;
+    
+    //TODO: mapear la relación
+    @OneToMany(mappedBy = "arriendo")
+    private Usuario creador;
+
+    @OneToOne(mappedBy = "arriendo")
+    private Direccion direccion;
+
+    //TODO: mapear caracteristicas
+
+    private Date creadoEn;
+    private Date actualizadoEn;
+
+    @PrePersist public void onCreate(){this.creadoEn = new Date();}
+    @PreUpdate public void onUpdate(){this.actualizadoEn = new Date();}
+
+    public Arriendo(){}
+    public Arriendo(Estado tipo_de_arriendo, boolean amoblado, int precio){
+        this.tipo_de_arriendo = tipo_de_arriendo;
+        this.amoblado = amoblado;
+        this.precio = precio;
+    }
+
+    public long getId(){return id;}
+    public void setId(long id){this.id = id;}
+    public Estado getTipo_de_arriendo(){return tipo_de_arriendo;}
+    public void setTipo_de_arriendo(Estado tipo_de_arriendo){this.tipo_de_arriendo = tipo_de_arriendo;}
+    public boolean getAmoblado(){return amoblado;}
+    public void setAmoblado(boolean amoblado){this.amoblado = amoblado;}
+    public int getPrecio(){return precio;}
+    public void setPrecio(int precio){this.precio = precio;}
+    public Usuario getCreador(){return creador;}
+    public void setCreador(Usuario creador){this.creador = creador;}
+    public Direccion getDireccion(){return direccion;}
+    public void setDireccion(Direccion direccion){this.direccion = direccion;}
+
+    public Date getCreadoEn(){return creadoEn;}
+    public Date getActualizadoEn(){return actualizadoEn;}
+    public void setActualizadoEn(Date actualizadoEn){this.actualizadoEn = actualizadoEn;}
+}
---
+++ src/main/java/com/chileroomie/chileroomieapp/modelos/Direccion.java
@@ -0,0 +1,53 @@
+package com.chileroomie.chileroomieapp.modelos;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "direcciones")
+public class Direccion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @NotBlank(message = "Por favor proporciona una dirección")
+    private String direccion;
+    @NotBlank(message = "Por favor proporciona una ciudad")
+    private String ciudad;
+    @NotBlank(message = "Por favor proporciona una comuna")
+    private String comuna;
+
+    // Espacio para asignar la relación
+
+    private Date creadoEn;
+    private Date actualizadoEn;
+
+    @PrePersist protected void onCreate(){this.creadoEn = new Date();}
+    @PreUpdate protected void onUpdate(){this.actualizadoEn = new Date();}
+
+    public Direccion(){}
+    public Direccion(String direccion, String ciudad, String comuna){
+        this.direccion = direccion;
+        this.ciudad = ciudad;
+        this.comuna = comuna;
+    }
+
+    public long getId(){return id;}
+    public void setId(long id){this.id = id;}
+    public String getDireccion(){return direccion;}
+    public void setDireccion(String direccion){this.direccion = direccion;}
+    public String getCiudad(){return ciudad;}
+    public void setCiudad(String ciudad){this.ciudad = ciudad;}
+    public String getComuna(){return comuna;}
+    public void setComuna(String comuna){this.comuna = comuna;}
+    public Date getCreadoEn(){return creadoEn;}
+    public Date getActualizadoEn(){return actualizadoEn;}
+    public void setActualizadoEn(Date actualizadoEn){this.actualizadoEn = actualizadoEn;}
+}
